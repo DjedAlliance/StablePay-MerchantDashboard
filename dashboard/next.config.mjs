dbb5eafd3888a9abcd6ff8374eedddc002dbd526
Siddharath <siddharthkj55@gmail.com>
2025-12-13 16:16:55 +0530

    Chore: Configure deployment workflow for GitHub Pages

--- dashboard/next.config.mjs
+++ dashboard/next.config.mjs
@@ -1,13 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  
+  output: 'export',
+  
+  
+  images: {
+    unoptimized: true,
+  },
+
+
+
   webpack: (config) => {
-    
     config.resolve.alias = {
       ...config.resolve.alias,
       '@react-native-async-storage/async-storage': false,
     };
 
-    
     config.resolve.fallback = {
       ...config.resolve.fallback,
       "pino-pretty": false,
