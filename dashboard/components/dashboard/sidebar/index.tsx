6260928cb55849f9390b3644a8a18da60a4121c0
MANISH <123263131+manishyad375375@users.noreply.github.com>
2025-12-13 15:11:58 +0530

    Update index.tsx

--- dashboard/components/dashboard/sidebar/index.tsx
+++ dashboard/components/dashboard/sidebar/index.tsx
@@ -69,7 +69,7 @@ export function DashboardSidebar({ className, ...props }: React.ComponentProps<t
       <SidebarHeader className="rounded-t-lg flex gap-3 flex-row rounded-b-none">
         <div className="flex overflow-clip size-24 shrink-0 items-center justify-center">
           <Image
-            src="/public/StablePay.svg"
+            src="/StablePay.svg"
             alt="StablePay Logo"
             width={80}
             height={80}
