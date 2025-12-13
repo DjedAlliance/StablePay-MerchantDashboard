654a480da1cafb476f47f4e34b77f206385b7763
MANISH <123263131+manishyad375375@users.noreply.github.com>
2025-12-13 14:24:11 +0530

    Update next.config.ts

--- dashboard/next.config.ts
+++ dashboard/next.config.ts
@@ -1,6 +1,12 @@
 import type { NextConfig } from "next"
 
+const isProd = process.env.NODE_ENV === 'production'
+const repoName = 'StablePay-MerchantDashboard'
+
 const nextConfig: NextConfig = {
+  output: 'export', // Enable static HTML export
+  basePath: isProd ? `/${repoName}` : '',
+  assetPrefix: isProd ? `/${repoName}/` : '',
   eslint: {
     ignoreDuringBuilds: true,
   },
